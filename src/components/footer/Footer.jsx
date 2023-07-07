9683e6f85e37f5f348432c1c0395fac9b7519709
denys <denia15042005@gmail.com>
2023-07-07 20:56:10 +0200

    first commit

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./footer.module.css";
+
+const Footer = () => {
+  return (
+    <div className={styles.container}>
+      &copy;2023 ViewAll. All rights reserved.
+    </div>
+  );
+};
+
+export default Footer;
